6b00d466bca8e41a30825b22bb5eb8346ccd63ee
jasur.kalandarov <jasur.kalandarov@danads.se>
2024-11-14 22:03:24 +0500

    tagged describe

--- tests/exercise_1.test.js
+++ tests/exercise_1.test.js
@@ -9,7 +9,7 @@ const {
 } = require('../src/1.String/exercise_1.js');
 const {expect} = require("chai");
 
-describe('Exercise 1', () => {
+describe('Exercise 1, @1', () => {
   describe('isStringEmpty Function', () => {
     it('isStringEmpty(\'abc\'); => false', () => {
       expect(isStringEmpty('abc')).to.equal(false);

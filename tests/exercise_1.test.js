ed9e05f4bd7d67c5997f0d4771c53d4168d33f7e
jasur.kalandarov <jasur.kalandarov@danads.se>
2024-11-15 12:26:30 +0500

    updated Exercise 1

--- tests/exercise_1.test.js
+++ tests/exercise_1.test.js
@@ -6,10 +6,10 @@ const {
   extractPriceFromText,
   createHashTag,
   changeTextCase
-} = require('../src/1.String/exercise_1.js');
+} = require('../src/exercise_1.js');
 const {expect} = require("chai");
 
-describe('Exercise 1, @1', () => {
+describe('Exercise @1', () => {
   describe('isStringEmpty Function', () => {
     it('isStringEmpty(\'abc\'); => false', () => {
       expect(isStringEmpty('abc')).to.equal(false);
